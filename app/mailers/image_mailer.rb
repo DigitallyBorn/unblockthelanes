0ec961d0452e3aba937fd0f8cda637e5a614b22b
Jordan Streiff <jordan.streiff@toolbox9.com>
2016-07-17 17:26:04 -0400

    add subjects

--- app/mailers/image_mailer.rb
+++ app/mailers/image_mailer.rb
@@ -1,11 +1,11 @@
 class ImageMailer < ApplicationMailer
   def success_email(email)
     @email = email
-    mail(to: @email, subject: 'success')
+    mail(to: @email, subject: 'Thanks for your submission!')
   end
   
   def fail_email(email)
     @email = email
-    mail(to: @email, subject: 'fail')
+    mail(to: @email, subject: 'Oops, something didn\'t work with your submission')
   end
 end
