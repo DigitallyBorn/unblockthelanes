619ef8ed5a5fa46ac17d42ab978d11b6ef4b6d95
Jordan Streiff <jordan.streiff@toolbox9.com>
2016-07-17 20:44:08 -0400

    add basic auth to emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,6 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: [:show, :edit, :update, :destroy]
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PW']
 
   # GET /emails
   # GET /emails.json
