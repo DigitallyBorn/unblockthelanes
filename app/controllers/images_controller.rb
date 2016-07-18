ec05ba50c0070f32111c7134513844d9871f8d39
Jordan Streiff <jordan.streiff@toolbox9.com>
2016-07-17 20:42:15 -0400

    add basic auth to images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PW']
 
   # GET /images
   # GET /images.json
